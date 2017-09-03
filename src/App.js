0811413ac37bf9bfae84c9cba9e2d965e95d85d4
Z Corbett <zyber17@gmail.com>
2017-09-02 23:58:06 -0400

    [feature] Sketched out the site in React

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
 import React from 'react'
 
-const App = () => <div className="App">Coming soon</div>
+const Messages = () => (
+	<div id="messages">
+		<ul>
+			<li>Hey, it's Z.</li>
+			<li>This site is coming soon.</li>
+			<li>Yay!~</li>
+		</ul>
+	</div>
+)
+
+const Suggestions = () => (
+	<div id="suggestions">
+		<ul>
+			<li>Exciting!</li>
+			<li>See you soon.</li>
+			<li>Yay!~</li>
+		</ul>
+	</div>
+)
+
+const Dialogue = () => (
+	<div id="dialogueWindow">
+		<Messages />
+		<Suggestions />
+	</div>
+)
+
+const App = () => <Dialogue />
 
 export default App
